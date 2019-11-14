018ad2f58346fa59b49bae9c4b5693f3d322e290
ci_arai <ci_arai@ci-arainoiMac.local>
2019-11-14 10:29:44 +0900

    add fish

---
+++ config.fish
@@ -0,0 +1,3 @@
+set -x PATH $HOME/.nodebrew/current/bin $PATH
+
+set -x ANDROID_HOME /Users/ci_arai/Library/Android/sdk
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,4 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
 
