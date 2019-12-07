72f7613b6df63ee12b403fe6edebc5c750aea5cd
新井辰弥 <araishinya@jPark.local>
2019-12-07 15:55:41 +0900

    add git alias

--- config.fish
+++ config.fish
@@ -1,3 +1,29 @@
 set -x PATH $HOME/.nodebrew/current/bin $PATH
 
 set -x ANDROID_HOME /Users/ci_arai/Library/Android/sdk
+
+
+# git alias
+alias g 'git'
+
+alias ga 'git add'
+alias ga. 'git add .'
+
+alias gb 'git branch'
+
+alias gcmsg 'git commit -m'
+
+alias gco 'git checkout'
+alias gcb 'git checkout -b'
+
+alias ggpull 'git pull origin'
+alias ggpush 'git push origin'
+
+alias gst 'git status'
+
+alias gsts 'git stash save'
+alias gsta 'git stash apply'
+alias gstc 'git stash clear'
+alias gstd 'git stash drop'
+alias gstl 'git stash list'
+alias gstp 'git stash pop'
